b0be9f5f3adc1eb6b766c61f77bf0af26bc428ec
spillerrec <spillerrec@hotmail.com>
2016-03-20 01:03:23 +0000

    ADD: Resize children attempting to optimize for squared shape

--- src/gui/FileItem.cpp
+++ src/gui/FileItem.cpp
@@ -24,21 +24,82 @@ void FileItem::initChildren(){
 	positionChildren();
 }
 
+
+class Positioner{
+	private:
+		QRectF area;
+		QPointF current;
+		std::vector<FileItem*> column;
+		int64_t total_mass;
+		int64_t current_mass{ 0 };
+		
+		
+		void addFileItem( FileItem* item ){
+			column.push_back( item );
+			current_mass += item->mass();
+		}
+		void clear(){
+			column.clear();
+			current_mass = 0;
+		}
+		Positioner( const Positioner& copy ) = default;
+		
+	public:
+		Positioner( QRectF area, int64_t total_mass )
+			: area(area), current(area.topLeft()), total_mass(total_mass) { }
+		
+		double columnWidth() const
+			{ return current_mass / double(total_mass) * area.width(); }
+			
+		double itemHeight( int64_t mass ) const
+			{ return area.height() * (mass / double(current_mass)); }
+		
+		double currentFit(){
+			double fit = 0.0;
+			for( auto file : column )
+				fit -= std::abs( 1.0 - columnWidth() / itemHeight( file->mass() ) );
+			return fit / column.size();
+		}
+		
+		/** Reszises the column, and moves to the next */
+		void commit(){
+			auto width = columnWidth();
+			double progress = current.y();
+			for( auto file : column ){
+				auto height = itemHeight( file->mass() );
+				file->setRect( current.x(), progress, width, height );
+				progress += height;
+			}
+			current.setX( current.x() + width );
+			
+			
+			clear();
+		}
+		
+		void addChild( FileItem* child ){
+			//Copy where this child have been added
+			auto added = Positioner(*this);
+			added.addFileItem( child );
+			
+			//Finalize column if it does not make sense to add more to it
+			if( added.currentFit() < currentFit() )
+				commit();
+			
+			addFileItem( child );
+		}
+};
+
 void FileItem::positionChildren(){
 	text->setPos( rect().topLeft() );
 	auto reserved = text->boundingRect().height();
 	
-	std::sort( children.begin(), children.end()
-		,	[](auto a, auto b){ return a->mass() > b->mass(); } );
+	auto biggest_first = [](auto a, auto b){ return a->mass() > b->mass(); };
+	std::sort( children.begin(), children.end(), biggest_first );
 	
-	double total = (rect().height()-reserved) / file->getTotalSize();
-	double progress = rect().y() + reserved;
-	for( auto child : children ){
-		double height = child->mass() * total;
-		child->setRect( rect().x(), progress, rect().width(), height );
-		progress += height;
-		child->positionChildren();
-	}
+	auto available = QRectF( rect().left(), rect().top() + reserved, rect().width(), rect().height() );
+	Positioner positioner( available, file->getTotalSize() );
+	for( auto child : children )
+		positioner.addChild( child );
 }
 
 int64_t FileItem::mass(){ return file->getTotalSize(); }
